084727fb8d7085bdf1a81739d27be52017299382
ayan <ayan1008@gmail.com>
2020-01-28 09:59:21 +0530

    -Detect objects in range

--- Archero/Assets/Scripts/PlayerController.cs
+++ Archero/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public int speed;
+    public float detectDistance = 10f;
+
     private Vector3 change;
     private Rigidbody2D _rigidbody;
     private DynamicJoystick _joystick;
@@ -29,19 +31,40 @@ public class PlayerController : MonoBehaviour
 #if UNITY_EDITOR
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-
 #elif UNITY_ANDROID
         change.x = _joystick.Horizontal;
         change.y = _joystick.Vertical;
 #endif
+
     }
 
     private void FixedUpdate()
+    {
+        DetectEnemy();
+        Move();
+    }
+
+    void Move()
     {
         if (change != Vector3.zero)
         {
             change.Normalize();
             _rigidbody.MovePosition(this.transform.position + change * speed * Time.deltaTime);
+        }
+    }
+
+    void DetectEnemy()
+    {
+        RaycastHit2D[] hit = Physics2D.CircleCastAll(this.transform.position, detectDistance, Vector2.right, detectDistance * 2f);
+        if (hit.Length > 0)
+        {
+            for (int i = 0; i < hit.Length; i++)
+            {
+                if (hit[i].collider != null)
+                {
+                    Debug.Log("hit" + hit[i].collider.name);
+                }
+            }
         }
     }
 }
